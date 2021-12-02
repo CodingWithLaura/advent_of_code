d2efbd5715c9f30ecc5112b08b7b99ae2cacc77b
CodingWithLaura <laura.ganss@web.de>
2021-12-02 09:56:31 +0100

    Access and sum up all values of forward movement

--- day_2/dive.py
+++ day_2/dive.py
@@ -1,7 +1,11 @@
 f = open("raw_dive_commands.txt", "r")
 lines = f.readlines()
 result = []
+max_forward = 0
 for x in lines:
    result.append(x.rstrip("\n").split(" ", 1))
-   print(result)
+for y in result:
+   if((y[0]) == 'forward'):
+      max_forward += sum(map(int, (y[1])))
+print(max_forward)
 f.close()
