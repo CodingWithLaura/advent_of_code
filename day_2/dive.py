f52c4af0815bf73676a5551ba4fe441221c8da54
CodingWithLaura <laura.ganss@web.de>
2021-12-02 10:11:07 +0100

    Calculate the final position using forward, up and down movements

--- day_2/dive.py
+++ day_2/dive.py
@@ -1,11 +1,23 @@
 f = open("raw_dive_commands.txt", "r")
 lines = f.readlines()
 result = []
-max_forward = 0
+horizontal_position = 0
+max_up = 0
+max_down = 0
+depth = 0
+final_position = 0
 for x in lines:
    result.append(x.rstrip("\n").split(" ", 1))
 for y in result:
    if((y[0]) == 'forward'):
-      max_forward += sum(map(int, (y[1])))
-print(max_forward)
+      horizontal_position += sum(map(int, (y[1])))
+   if((y[0]) == 'up'):
+      max_up += sum(map(int, (y[1])))
+   if((y[0]) == 'down'):
+      max_down += sum(map(int, (y[1])))
+print(horizontal_position)
+print(max_down - max_up)
+depth = max_down - max_up
+final_position = horizontal_position * depth
+print(final_position)
 f.close()
