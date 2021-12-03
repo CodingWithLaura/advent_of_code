f2365e72a9054a38b4cc18f8cf54e463cb0eb205
CodingWithLaura <laura.ganss@web.de>
2021-12-03 17:19:32 +0100

    Use nested for loops to calculate amounts of zeros and ones per column and compaire them

--- day_3/binary_diagnostics.py
+++ day_3/binary_diagnostics.py
@@ -1,8 +1,15 @@
-f = open("test.txt", "r")
+
+def bin_to_decimal(array):
+    ergebnis = 0
+    length = len(array)
+    for number in range(length):
+        print(array[(length -1) - number])
+        ergebnis += array[(length -1) - number]  * (2 ** number) 
+    return ergebnis
+
+f = open("binary_numbers.txt", "r")
 lines = f.readlines()
 result = []
-akku_zero = 0
-akku_one = 0
 list_zero = []
 list_one = []
 
@@ -11,27 +18,49 @@ for x in lines:
     result.append(x.rstrip("\n"))
 print(result)
 
-for y in result:
-    my_list = []
-    if(y[0] == "0"):
-         print("foo")
-         akku_zero += 1
-    if(y[0] == "1"):
-        print("bar")
-        akku_one += 1
-print(akku_zero)
-print(akku_one)
-my_list = []
-if(akku_zero > akku_one):
-    my_list.append(akku_zero)
-else:
-    my_list.append(akku_one)
-print(my_list)
+length_of_row = len(result[0])
+for column in range (length_of_row):
+    akku_zero = 0
+    akku_one = 0
+    for row in result:    
+        if(row[column] == "0"):
+            akku_zero += 1
+        else:
+            akku_one += 1
+    list_zero.append(akku_zero);
+    list_one.append(akku_one);
+print(list_zero)
+print("\n")
+print(list_one)
+
+final_max_array = []
+final_min_array = []
+for number in range (length_of_row):
+    if list_zero[number] > list_one[number]:
+        final_max_array.append(0)
+        final_min_array.append(1)
+    else:
+        final_max_array.append(1)
+        final_min_array.append(0)
+print("\n")
+print(final_max_array)
+print(final_min_array)
+
+first_decimal = bin_to_decimal(final_max_array)
+second_decimal = bin_to_decimal(final_min_array)
+
+result = first_decimal * second_decimal
+
+print("\n")
+print("result:{} first: {} second: {}".format(result,first_decimal,second_decimal))
+
 
     
 
 
-    
-    
-    
-    
+
+
+
+
+
+
