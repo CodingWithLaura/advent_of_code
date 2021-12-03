99209528fa6b778bee0f07c9e76e7e38a8238b89
CodingWithLaura <laura.ganss@web.de>
2021-12-03 11:59:17 +0100

    Iterate through first entry of every sublist and count zeros and ones, highest value gets appended to list

--- day_3/binary_diagnostics.py
+++ day_3/binary_diagnostics.py
@@ -3,18 +3,34 @@ lines = f.readlines()
 result = []
 akku_zero = 0
 akku_one = 0
-i = 0
+list_zero = []
+list_one = []
+
 
 for x in lines:
     result.append(x.rstrip("\n"))
 print(result)
+
 for y in result:
+    my_list = []
     if(y[0] == "0"):
-        akku_zero += 1
+         print("foo")
+         akku_zero += 1
     if(y[0] == "1"):
+        print("bar")
         akku_one += 1
 print(akku_zero)
 print(akku_one)
+my_list = []
+if(akku_zero > akku_one):
+    my_list.append(akku_zero)
+else:
+    my_list.append(akku_one)
+print(my_list)
+
+    
+
+
     
     
     
