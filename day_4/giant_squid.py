80570d2f566038bd226f95fec6ed8a54aba63248
CodingWithLaura <laura.ganss@web.de>
2021-12-04 11:15:59 +0100

    Read bingo numbers from textfile

--- day_4/giant_squid.py
+++ day_4/giant_squid.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 first_board = np.loadtxt('example_boards.txt',dtype=int, max_rows=5)
-print(first_board)
-print(first_board[0])
-print(first_board[1])
 
 first_board_swap = np.swapaxes(first_board, 0, 1)
-print(first_board_swap)
-print(first_board_swap[0])
-print(first_board_swap[1])
 
 second_board = np.loadtxt('example_boards.txt', dtype=int, skiprows=5, max_rows=5)
 
 third_board = np.loadtxt('example_boards.txt', dtype=int, skiprows=11, max_rows=5)
 
+f = open("example_input.txt", "r")
+lines = f.readlines()
+bingo_nums = []
+
+for x in lines:
+    bingo_nums.append(x.rstrip("\n").split(","))
+print(bingo_nums)
+
+
+for i in range (len(first_board[0])):
+   print(first_board[i])
