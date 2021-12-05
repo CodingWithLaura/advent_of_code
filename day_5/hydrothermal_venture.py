6e7b48f15878a6b6c330bb07adde0af1bbcb2d3d
CodingWithLaura <laura.ganss@web.de>
2021-12-05 14:25:22 +0100

    Filter all horizontal and vertical lines and put them in new list

--- day_5/hydrothermal_venture.py
+++ day_5/hydrothermal_venture.py
@@ -4,6 +4,11 @@ with open ('test_numbers.txt', 'r') as file:
     for line in file.readlines():
         f_list = [int(i) for i in line.rstrip('\n').replace(' -> ', ',').split( ',')]
         my_data.append(f_list)
-
-
 print(my_data)
+
+new_list = []
+for list in my_data:
+    if(list[0] == list[2] or list[1] == list[3]):
+        new_list.append(list)
+print(new_list)
+        
