8bb63fa5b685f30d3457f7376a2d9618e58ecc12
CodingWithLaura <laura.ganss@web.de>
2021-12-05 21:53:29 +0100

    Implement list which contains all points and dictonary to count occurences of each point

--- day_5/hydrothermal_venture.py
+++ day_5/hydrothermal_venture.py
@@ -1,25 +1,63 @@
-my_data = []
+def get_venture_points(venture_line):
+    #[x1,y1,x2,y2]  #horizontal oder vertikal???
+    points = []
+    if(venture_line[0] == venture_line[2]): #horizontal
+        x = venture_line[0]
+        y1 = min(venture_line[1], venture_line[3])
+        y2 = max(venture_line[1], venture_line[3])
+        for y in range(y1,y2+1):
+            points.append((x,y)) 
+    if(venture_line[1] == venture_line[3]): #verical
+        y = venture_line[1]
+        x1 = min(venture_line[0],venture_line[2])
+        x2 = max(venture_line[0],venture_line[2])
+        for x in range(x1,x2+1):
+            points.append((x,y))
+    return points
 
-with open ('test_numbers.txt', 'r') as file:
+my_data = []
+with open ('real_numbers.txt', 'r') as file:
     for line in file.readlines():
         f_list = [int(i) for i in line.rstrip('\n').replace(' -> ', ',').split(',')]
         my_data.append(f_list)
-print(my_data)
+#print(my_data)
 
-new_list = []
+venture_list = []
 for list in my_data:
    if(list[0] == list[2] or list[1] == list[3]):
-       new_list.append(list)
-print(new_list)
+       venture_list.append(list)
+#print(venture_list)
 
-distance_y = []
-distance_x = []
-for list in new_list:
-        if(list[0] == list[2]):
-            print("yes")
-            distance_y.append(list[1] - list[3])
-        elif(list[1] == list[3]):
-            print("ohyes")
-            distance_x.append(list[0] - list[2])
-print(distance_y)
-print(distance_x)
+venture_points_list = []
+
+for venture_line in venture_list:
+    points = get_venture_points(venture_line)
+    venture_points_list.append(points)
+#print(venture_points_list)
+
+# haben jetzt ne liste von allen punkten
+# wann sind die mindestens doppelt???
+
+duplicate_dict = {}
+for points in venture_points_list:
+    for (x,y) in points: #punkt aus liste von punkten ienes ventures
+        isdrin_x = duplicate_dict.get(x)
+        if(isdrin_x == None): #
+            duplicate_dict[x] = {y:1}
+        else:
+            isdrin_y = duplicate_dict[x].get(y)
+            if(isdrin_y == None):
+                duplicate_dict[x][y] = 1
+            else:
+                duplicate_dict[x][y] += 1
+
+#print(duplicate_dict)
+
+amount_dangerous_ventures = 0
+for y_dicts_keys in duplicate_dict:
+    for x_keys in duplicate_dict[y_dicts_keys]:
+        value = duplicate_dict[y_dicts_keys][x_keys]
+        if(value > 1):
+            amount_dangerous_ventures += 1
+print(amount_dangerous_ventures)            
+            
