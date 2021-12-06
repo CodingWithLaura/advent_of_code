26dbd54aa6ee00c82b9451ead8c544161ddcbe2b
CodingWithLaura <laura.ganss@web.de>
2021-12-06 12:36:48 +0100

    Calculate how many laternfish are there after 80 days

--- day_6/laternfish.py
+++ day_6/laternfish.py
@@ -1,20 +1,16 @@
 laternfish = []
 
-f = open("test_numbers.txt", "r")
+f = open("real_numbers.txt", "r")
 for line in f.readlines():
     fields = line.split(',')
     for i in range(len(fields)):
         laternfish.append(int(fields[i]))
 
-print(laternfish)
-
-for i in range (0, 18):
+for i in range (0, 80):
     for x in range (len(laternfish)):
         if(laternfish[x] == 0):
             laternfish[x] = 6
-            laternfish.insert(laternfish[x], 8)
+            laternfish.append(8)
         else:
             laternfish[x] -= 1
-    print(laternfish)
-
 print(len(laternfish))
