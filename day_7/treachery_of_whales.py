2fc6ce83e01910b630ce831cac9bc0555daa743b
CodingWithLaura <laura.ganss@web.de>
2021-12-07 16:50:58 +0100

    Implement nested for loops which compaire each value of list with itself and sums up absolute values

--- day_7/treachery_of_whales.py
+++ day_7/treachery_of_whales.py
@@ -5,12 +5,17 @@ for line in f.readlines():
     fields = line.split(',')
     for i in range(len(fields)):
         crab_list.append(int(fields[i]))
-print(crab_list)
 
 
 solution = []
 for i in range(len(crab_list)):
            solution.append(crab_list[0] - crab_list[i])
-print(solution)
 final = sum(solution)
-print(final)
+
+real_list = []
+for j in range(len(crab_list)):
+    temp_list = []
+    for i in range (  len(crab_list)):
+        temp_list.append(abs(crab_list[j] - crab_list[i]))
+    print((sum(temp_list)))
+        
