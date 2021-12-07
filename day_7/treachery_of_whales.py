53aa21b7e7730f86e94ede65c8460508e3b39007
CodingWithLaura <laura.ganss@web.de>
2021-12-07 17:00:56 +0100

    Print index where diff between list and each its items are the lowest

--- day_7/treachery_of_whales.py
+++ day_7/treachery_of_whales.py
@@ -1,21 +1,18 @@
 crab_list = []
 
-f = open("test_numbers.txt", "r")
+f = open("real_numbers.txt", "r")
 for line in f.readlines():
     fields = line.split(',')
     for i in range(len(fields)):
         crab_list.append(int(fields[i]))
 
-
-solution = []
-for i in range(len(crab_list)):
-           solution.append(crab_list[0] - crab_list[i])
-final = sum(solution)
-
 real_list = []
 for j in range(len(crab_list)):
     temp_list = []
-    for i in range (  len(crab_list)):
+    for i in range (len(crab_list)):
         temp_list.append(abs(crab_list[j] - crab_list[i]))
-    print((sum(temp_list)))
+        sum_list = (sum(temp_list))
+    real_list.append(sum_list)
+min_value = (min(real_list))
+print(min_value)
         
