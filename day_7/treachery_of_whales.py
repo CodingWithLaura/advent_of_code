f93f46b0404493093bec352b09f02530b074c07f
CodingWithLaura <laura.ganss@web.de>
2021-12-07 09:01:48 +0100

    Compare first element of list with all others and sum up differences

--- day_7/treachery_of_whales.py
+++ day_7/treachery_of_whales.py
@@ -6,3 +6,11 @@ for line in f.readlines():
     for i in range(len(fields)):
         crab_list.append(int(fields[i]))
 print(crab_list)
+
+
+solution = []
+for i in range(len(crab_list)):
+           solution.append(crab_list[0] - crab_list[i])
+print(solution)
+final = sum(solution)
+print(final)
