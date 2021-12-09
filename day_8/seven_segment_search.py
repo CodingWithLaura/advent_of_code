5552f67a98b6da5751e560074278ff3abe4c60bd
CodingWithLaura <laura.ganss@web.de>
2021-12-09 12:53:25 +0100

    Format lists so that each string is an item and search and ten digits are seperated

--- day_8/seven_segment_search.py
+++ day_8/seven_segment_search.py
@@ -4,5 +4,25 @@ f = open('test.numbers.txt', 'r')
 lines = f.readlines()
 
 for x in lines:
-    digits.append(x.rstrip("|\n ").split(" "))
-print(digits)
+    digits.append(x.rstrip("|\n ").replace(" ", ",").split(" "))
+
+flat_list = []
+for sublist in digits:
+    for item in sublist:
+        flat_list.append(item)
+
+ten_digits = []
+search_digits = []
+for i in range (len(flat_list)):
+    if(i % 2 == 0):
+        ten_digits.append(flat_list[i].split(","))
+    else:
+        search_digits.append(flat_list[i].split(","))
+print(ten_digits[0])
+
+
+
+
+
+
+
