4c22b268ec70d63008d940fc7103ee52650de7b8
CodingWithLaura <laura.ganss@web.de>
2021-12-09 18:47:23 +0100

    Search for unique digits by filtering string length

--- day_8/seven_segment_search.py
+++ day_8/seven_segment_search.py
@@ -1,28 +1,40 @@
 digits = []
 
-f = open('test.numbers.txt', 'r')
+f = open('real.numbers.txt', 'r')
 lines = f.readlines()
 
 for x in lines:
-    digits.append(x.rstrip("|\n ").replace(" ", ",").split(" "))
+    digits.append(x.replace(" ", ",").replace("\n", "").split(" "))
 
 flat_list = []
-for sublist in digits:
+for line in digits:
+    flat_line_list = []
+    sublist = line[0].split(',')
     for item in sublist:
-        flat_list.append(item)
-
+        flat_line_list.append(item)
+    flat_list.append(flat_line_list)    
+      
 ten_digits = []
 search_digits = []
-for i in range (len(flat_list)):
-    if(i % 2 == 0):
-        ten_digits.append(flat_list[i].split(","))
-    else:
-        search_digits.append(flat_list[i].split(","))
-print(ten_digits[0])
-
-
-
-
-
-
+for line in flat_list:
+    ten_line_digits = []
+    search_line_digits = []
+    pipe_found = False
+    for item in line:
+        if(item == '|'):
+            pipe_found = True
+        else:
+            if pipe_found:
+                search_line_digits.append(item)
+            else:
+                ten_line_digits.append(item)
+    ten_digits.append(ten_line_digits)
+    search_digits.append(search_line_digits)
+    
+result = 0
+for x in search_digits:
+    for y in x:
+        if(len(y) == 2 or len(y) == 3 or len(y) == 4 or len(y) == 7):         
+            result += 1
+print(result)
 
